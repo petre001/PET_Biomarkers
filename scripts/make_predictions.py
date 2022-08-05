864875042a436e3500367768cfa4f46fcf006d84
petre001 <jeffrey.petrella@duke.edu>
2022-08-05 14:58:11 -0400

    Update on python scripts

--- scripts/make_predictions.py
+++ scripts/make_predictions.py
@@ -1,8 +1,9 @@
-def visualize_results(model, dataloader, batch_size, device):
+def visualize_results(model, images, labels, batch_size, device):
     # Display a batch of predictions
     import numpy as np
     import matplotlib.pyplot as plt
     import torch
+    import streamlit as st
 
     model = model.to(device)  # Send model to GPU if available
     batch_size = batch_size
@@ -15,7 +16,7 @@ def visualize_results(model, dataloader, batch_size, device):
     with torch.no_grad():
         model.eval()
         # Get a batch of demo images
-        images, labels = next(iter(dataloader))
+        #images, labels = next(iter(dataloader))
         images, labels = images.to(device), labels.to(device)
         # Get predictions
         _, preds = torch.max(model(images), 1)
@@ -23,11 +24,11 @@ def visualize_results(model, dataloader, batch_size, device):
         labels = labels.cpu().numpy()
         images = images.cpu().numpy()  # Convert images to numpy for display
 
-    print(images[:, 0, 40, :, :][0].shape)
+    #print(images[:, 0, 40, :, :][0].shape)
     print(preds)
 
     # plot the images in the batch, along with the corresponding labels
-    fig = plt.figure(figsize=(4, 8))
+    fig = plt.figure(figsize=(batch_size/2, batch_size/1.5))
     for idx in range(batch_size):
         ax = fig.add_subplot(int(batch_size/2), 2, idx + 1, xticks=[], yticks=[])
         # ax.imshow(np.rot90(im[:,0,40,:,:][idx]), cmap='gray')
@@ -35,5 +36,6 @@ def visualize_results(model, dataloader, batch_size, device):
         # ax.set_title(idx_to_class[label[idx]])
         ax.set_title("{} ({})".format(idx_to_class[preds[idx]], idx_to_class[labels[idx]]),
                      color=("green" if preds[idx] == labels[idx] else "red"))
-    plt.show()
+    #plt.show()
+    st.pyplot(fig=fig)
     return
